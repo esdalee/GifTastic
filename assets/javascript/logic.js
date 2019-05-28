aec5215c4e88482190c114ea62d5f7c68cf5bc78
esdalee <es.da.lee@gmail.com>
2019-05-27 20:37:44 -0400

    create and call renderButtons function

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,9 +2,12 @@ $(document).ready(function() {
 
 
     var APIKey = "YuLShxF8h1Z5K6nn3rENeDtBLj8x5q5E";
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=" + APIKey;
+    var queryURL = "https://api.giphy.com/v1/gifs/random?limit=10&api_key=" + APIKey;
 
     var topics = ["Suits", "Killing Eve", "Big Little Lies", "The Office", "Friends", "Sherlock", "Marvelous Mrs.Maisel", "Black Mirror", "The Blacklist", "The Good Place"];
+
+    // Load initial buttons
+    renderButtons();
 
     // AJAX call to Giphy API
     $.ajax({
@@ -14,5 +17,20 @@ $(document).ready(function() {
       console.log(response);
     });
 
+    function renderButtons() {
+        // Delete previous buttons to avoid repeat
+        $("#buttons-list").empty();
+        for (var i = 0; i < topics.length; i++) {
+            var showDiv = $("<button>");
+            showDiv.addClass("tvshow");
+            // Add attribute with value of TV show
+            showDiv.attr("data-name", topics[i]);
+            // Add TV show name
+            showDiv.text(topics[i]);
+            // Adding the button to the HTML
+            $("#buttons-list").append(showDiv);
+        
+        }
+    };
 
 });
