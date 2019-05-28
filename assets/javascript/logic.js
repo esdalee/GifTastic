882176930919922e2da27a203d2c15b14e343fd9
esdalee <es.da.lee@gmail.com>
2019-05-27 22:24:35 -0400

    AJAX call to retrive gifs

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,22 +1,15 @@
 $(document).ready(function() {
 
-    // Set query URL and APIKey
+    // Set variables
     var APIKey = "YuLShxF8h1Z5K6nn3rENeDtBLj8x5q5E";
     var queryURL = "https://api.giphy.com/v1/gifs/random?limit=10&api_key=" + APIKey;
+    var queryTerm = "";
 
     // Initialize variable
     var topics = ["Suits", "Killing Eve", "Big Little Lies", "The Office", "Friends", "Sherlock", "Marvelous Mrs.Maisel", "Black Mirror", "The Blacklist", "The Good Place"];
 
-    // Load initial buttons
+    // Load buttons from initialized array
     renderButtons();
-
-    // AJAX call to Giphy API
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-    });
 
     // Renders tv show buttons
     function renderButtons() {
@@ -27,7 +20,7 @@ $(document).ready(function() {
             var showDiv = $("<button>");
             showDiv.addClass("tvshow");
             // Add attribute with value of TV show
-            showDiv.attr("data-name", topics[i]);
+            showDiv.attr("name", topics[i]);
             // Add TV show name
             showDiv.text(topics[i]);
             // Adding the button to the HTML
@@ -36,7 +29,29 @@ $(document).ready(function() {
         }
     };
 
-    // Click handler for adding tv shows
+    // Call AJAX and retrieve gifs
+    function runQuery(queryURL) {
+        // AJAX call to Giphy API
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(gifData) {
+            console.log(gifData);
+        });
+    }
+
+    // Click handler to generate new query URL
+    $(".tvShow").on("click", function() {
+        queryTerm = $(this).val().trim();
+        console.log(queryTerm);
+        var newURL = queryURL + "&q=" + queryTerm;
+        console.log(newURL);
+        // Send AJAX call with new uRL
+        runQuery(newURL);
+    });
+
+
+    // Click handler to add tv show buttons
     $("#add-tvShow").on("click", function(e) {
          e.preventDefault();
         // Get value from user's input
